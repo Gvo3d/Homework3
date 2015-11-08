a33ee615cb3c42d8d47429ebbbbfdac0e3b5ad47
Gvo3d <denis.yakimov89@gmail.com>
2015-11-08 20:03:19 +0200

    First version

---
+++ src/MoneyCalculator/Application.java
@@ -0,0 +1,58 @@
+package MoneyCalculator;
+/**
+ * Made by Yakimov Denis, 08/11/2015 for Hillel Java course       Homework 2
+ */
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("Money Calculator");
+        System.out.println("For Hillel Java Courses by Yakimov Denis");
+        System.out.println("Enter cash sum, one-year-percentage and number of years.");
+        double CashSum = 0;
+        byte Percentage = 0;
+        byte Years = 0;
+        double Aliments=0;
+
+        //Main while-loop without break
+        for (int i = 0; i <= 3; i++) {
+            Scanner InputScanner = new Scanner(System.in);
+            switch (i) {
+                case 0:
+                    if (InputScanner.hasNextDouble()) {
+                        CashSum = InputScanner.nextDouble();
+                    } else Ending();
+                    break;
+                case 1:
+                    if (InputScanner.hasNextByte()) {
+                        Percentage = InputScanner.nextByte();
+                    } else Ending();
+                    break;
+                case 2:
+                    if (InputScanner.hasNextByte()) {
+                        Years = InputScanner.nextByte();
+                    } else Ending();
+                    break;
+            }
+        }
+
+            for (i = 0; i <= Years; i++) {
+                Aliments = CashSum*Percentage/100;
+                CashSum = CashSum+Aliments;
+                System.out.println("Year " + i);
+                System.out.println("Cash sum is " +CashSum);
+                System.out.println("Percentage value for a year is "+Aliments);
+                System.out.println();
+            }
+        System.exit(0);
+    }
+
+    public static void Ending() {
+        System.out.println("ERROR IN STATEMENT!");
+        System.exit(0);
+    }
+
+
+}
