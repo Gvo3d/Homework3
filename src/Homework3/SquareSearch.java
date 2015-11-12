8dce863ff77f3cfc80239cf2695b55e3624c126e
Gvo3d <denis.yakimov89@gmail.com>
2015-11-12 17:00:33 +0200

    +square

--- src/Homework3/SquareSearch.java
+++ src/Homework3/SquareSearch.java
@@ -1,5 +1,7 @@
 package Homework3;
+
 import java.util.Scanner;
+
 /**
  * Made by Yakimov Denis, 11/11/2015 for Hillel Java course       Homework 3
  */
@@ -8,19 +10,30 @@ public class SquareSearch {
         System.out.println("SquareSearch");
         System.out.println("Made by Yakimov Denis for Hillel Java Courses");
         System.out.println("Input two point(X and Y coordinates for both).");
-        int number = 0;
-        int counter=0;
-        int ax,ay,bx,by;
+        int ax, ay, bx, by,cx,cy;
 
-        while (number == 0) {
-            System.out.println("Enter an valid integer number");
-            Scanner in = new Scanner(System.in);
-            if (in.hasNextInt()) {
-                ax=number;
-                number = in.nextInt();
-            } else System.out.println("Error in data input, reinput number!");
+        ax=GetNumber();
+        ay=GetNumber();
+        bx=GetNumber();
+        by=GetNumber();
+        System.out.println("Enter a point location!");
+        cx=GetNumber();
+        cy=GetNumber();
+
+        if ((bx>=cx) && (cx>=ax) &&(by>=cy) && (cy>=ay)) {
+            System.out.println("Point is in square!");
+        } else System.out.println("Point is not in a square!");
         }
 
-
+    public static int GetNumber() {
+        int number=0;
+        System.out.println("Enter an valid integer number");
+        Scanner in = new Scanner(System.in);
+        if (in.hasNextInt()) {
+            number = in.nextInt();
+        } else System.out.println("Error in data input, reinput number!");
+        return number;
     }
+
+
 }
